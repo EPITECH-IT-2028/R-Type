617f301d8093e1e118070da586bbb67562864026
Nolann Dubos <nolann.dubos@epitech.eu>
2025-09-18 15:41:09 +0200

    fix: unordered map in the hpp

--- server/src/Packets/PacketFactory.hpp
+++ server/src/Packets/PacketFactory.hpp
@@ -4,6 +4,8 @@
 #include <memory>
 #include <unordered_map>
 #include "APacket.hpp"
+#include "Packet.hpp"
+#include "PacketHandler.hpp"
 
 namespace packet {
 
@@ -15,9 +17,14 @@ namespace packet {
       std::unique_ptr<APacket> createHandler(uint8_t packetType);
 
     private:
-      static const std::unordered_map<uint8_t,
-                                      std::function<std::unique_ptr<APacket>()>>
-          _handlers;
+      inline static const std::unordered_map<
+          uint8_t, std::function<std::unique_ptr<APacket>()>>
+          _handlers = {{static_cast<uint8_t>(PacketType::Message),
+                        []() { return std::make_unique<MessageHandler>(); }},
+                       {static_cast<uint8_t>(PacketType::PlayerInfo),
+                        []() { return std::make_unique<PlayerInfoHandler>(); }},
+                       {static_cast<uint8_t>(PacketType::Position),
+                        []() { return std::make_unique<PositionHandler>(); }}};
   };
 
 }  // namespace packet
