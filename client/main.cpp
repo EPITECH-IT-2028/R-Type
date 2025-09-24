293d1425c4b21b8e4b993697a812cd1e13de3d04
Quentin L. <quentin.lapierre@epitech.eu>
2025-09-24 16:05:24 +0200

    fix(client): set FPS only if vsync is not enabled
    Previously, SetTargetFPS(60) was always called, which could conflict
    with
    the VSYNC setting. Now, FPS is set to 60 only if VSYNC is not enabled,
    preventing redundant or conflicting frame rate settings.

--- client/main.cpp
+++ client/main.cpp
@@ -3,7 +3,8 @@
 int main() {
   SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
   InitWindow(800, 600, "Raylib Window");
-  SetTargetFPS(60);
+  if (!IsWindowState(FLAG_VSYNC_HINT))
+    SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
