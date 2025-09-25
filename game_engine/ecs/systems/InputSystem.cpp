9de98b32fb916841580829bde860d67ccd64b219
Quentin L. <quentin.lapierre@epitech.eu>
2025-09-25 13:10:42 +0200

    feat(input): add InputSystem for handling key events
    Introduced InputSystem to process keyboard input and update entity
    velocities accordingly. The system checks for arrow key presses and
    modifies the VelocityComponent of each entity, enabling movement
    control via keyboard. This lays the groundwork for player input
    handling in the ECS architecture.

---
+++ game_engine/ecs/systems/InputSystem.cpp
@@ -0,0 +1,27 @@
+#include "InputSystem.hpp"
+#include "components/VelocityComponent.hpp"
+#include "raylib.h"
+
+namespace ecs {
+  void InputSystem::update(float deltaTime) {
+    for (auto const &entity : _entities) {
+      auto &velocity = _ecsManager.getComponent<VelocityComponent>(entity);
+
+      if (IsKeyDown(KEY_UP)) {
+        velocity.vy = -1;
+      } else if (IsKeyDown(KEY_DOWN)) {
+        velocity.vy = 1;
+      } else {
+        velocity.vy = 0;
+      }
+
+      if (IsKeyDown(KEY_LEFT)) {
+        velocity.vx = -1;
+      } else if (IsKeyDown(KEY_RIGHT)) {
+        velocity.vx = 1;
+      } else {
+        velocity.vx = 0;
+      }
+    }
+  }
+}  // namespace ecs
