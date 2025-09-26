ed81f21cbac8f4bacaf390d48475e67310eb5ab3
Quentin L. <quentin.lapierre@epitech.eu>
2025-09-26 18:36:37 +0200

    feat(input): add speed and position update to InputSystem
    InputSystem now multiplies velocity by entity speed and updates position
    based on velocity and deltaTime. This enables smoother and more accurate
    movement handling for entities with PositionComponent and
    SpeedComponent.

--- game_engine/ecs/systems/InputSystem.cpp
+++ game_engine/ecs/systems/InputSystem.cpp
@@ -1,19 +1,26 @@
 #include "InputSystem.hpp"
+#include "components/PositionComponent.hpp"
+#include "components/SpeedComponent.hpp"
 #include "components/VelocityComponent.hpp"
 #include "raylib.h"
 
 namespace ecs {
   void InputSystem::update(float deltaTime) {
-    (void)deltaTime;
     for (auto const &entity : _entities) {
       auto &velocity = _ecsManager.getComponent<VelocityComponent>(entity);
+      auto &position = _ecsManager.getComponent<PositionComponent>(entity);
+      auto const &speed = _ecsManager.getComponent<SpeedComponent>(entity);
 
       const int velocityY =
           (IsKeyDown(KEY_DOWN) ? 1 : 0) - (IsKeyDown(KEY_UP) ? 1 : 0);
       const int velocityX =
           (IsKeyDown(KEY_RIGHT) ? 1 : 0) - (IsKeyDown(KEY_LEFT) ? 1 : 0);
-      velocity.vy = velocityY;
-      velocity.vx = velocityX;
+
+      velocity.vy = velocityY * speed.speed;
+      velocity.vx = velocityX * speed.speed;
+
+      position.x += velocity.vx * deltaTime;
+      position.y += velocity.vy * deltaTime;
     }
   }
 }  // namespace ecs
