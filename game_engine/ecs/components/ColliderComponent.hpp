fe37b8c69e94be08b7ad280162c582fe2e553504
ZifFiji <arthur.guerinault@epitech.eu>
2025-10-01 16:08:59 +0200

    feat: collider component

---
+++ game_engine/ecs/components/ColliderComponent.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+struct Vec2 {
+    float x;
+    float y;
+};
+
+namespace ecs {
+
+  /** @brief Component that defines a collider for an entity.
+   * It can be an axis-aligned bounding box (AABB).
+   */
+  struct ColliderComponent {
+      Vec2 center = {0.0f, 0.0f};
+      Vec2 halfSize = {0.5f, 0.5f};
+  };
+}  // namespace ecs
