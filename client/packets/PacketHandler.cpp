8e68cd121be931c4c360a2a563d8c4ebd19d511d
Quentin L. <quentin.lapierre@epitech.eu>
2025-10-02 09:54:35 +0200

    refactor(client): remove braces from single-line if
    Simplified the handlePacket function in PacketHandler.cpp by removing
    unnecessary braces from a single-line if statement for improved code
    readability and consistency with style guidelines.

--- client/packets/PacketHandler.cpp
+++ client/packets/PacketHandler.cpp
@@ -4,9 +4,8 @@
 
 int packet::MessageHandler::handlePacket(client::Client &client,
                                          const char *data, std::size_t size) {
-  if (size < sizeof(MessagePacket)) {
+  if (size < sizeof(MessagePacket))
     return 84;
-  }
 
   const MessagePacket *packet = reinterpret_cast<const MessagePacket *>(data);
   std::cout << "[MESSAGE] From server: " << packet->message << std::endl;
