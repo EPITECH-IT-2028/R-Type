6002629d5500d161f1100f2f2fc9a0ed4ea8f595
Chassagne <robin.chassagne@epitech.eu>
2025-10-06 11:48:01 +0200

    feat: every methods to animate the Sprite

--- game_engine/ecs/systems/SpriteAnimationSystem.cpp
+++ game_engine/ecs/systems/SpriteAnimationSystem.cpp
@@ -7,10 +7,8 @@ void ecs::SpriteAnimationSystem::update(float deltaTime) {
     
     if (!animation.isPlaying) {
         continue;
-    }
-            
+    } 
     animation.frameTimer += deltaTime;
-            
     if (animation.frameTimer >= animation.frameTime) {
       animation.currentFrame++;
       animation.frameTimer = 0.0f;
@@ -23,7 +21,7 @@ void ecs::SpriteAnimationSystem::update(float deltaTime) {
           animation.isPlaying = false;
         }
       }
-      sprite.sourceRect = animation.getCurrentFrameRect();
+      sprite.sourceRect = getCurrentFrameRect(entity);
     }
   }
 }
@@ -32,14 +30,69 @@ void ecs::SpriteAnimationSystem::setSelectedRow(Entity entity, int row) {
   auto& animation = _ecsManager.getComponent<SpriteAnimationComponent>(entity);
   auto& sprite = _ecsManager.getComponent<SpriteComponent>(entity);
   
-  animation.setSelectedRow(row);
-  sprite.sourceRect = animation.getCurrentFrameRect();        
+  if (row >= 0 && row < animation.totalRows) {
+    animation.selectedRow = row;
+    animation.currentFrame = animation.startFrame;
+    animation.frameTimer = 0.0f;
+  }
+  sprite.sourceRect = getCurrentFrameRect(entity);
+}
+
+void ecs::SpriteAnimationSystem::setAnimationRange(Entity entity, int start, int end) {
+  auto& animation = _ecsManager.getComponent<SpriteAnimationComponent>(entity);
+  
+  animation.startFrame = start;
+  animation.endFrame = end;
+  animation.currentFrame = start;
+  animation.frameTimer = 0.0f;
+}
+
+void ecs::SpriteAnimationSystem::play(Entity entity) {
+  auto& animation = _ecsManager.getComponent<SpriteAnimationComponent>(entity);
+  animation.isPlaying = true;
+}
+
+void ecs::SpriteAnimationSystem::pause(Entity entity) {
+  auto& animation = _ecsManager.getComponent<SpriteAnimationComponent>(entity);
+  animation.isPlaying = false;
+}
+
+void ecs::SpriteAnimationSystem::stop(Entity entity) {
+  auto& animation = _ecsManager.getComponent<SpriteAnimationComponent>(entity);
+  animation.isPlaying = false;
+  animation.currentFrame = animation.startFrame;
+  animation.frameTimer = 0.0f;
+}
+
+void ecs::SpriteAnimationSystem::restart(Entity entity) {
+  auto& animation = _ecsManager.getComponent<SpriteAnimationComponent>(entity);
+  animation.currentFrame = animation.startFrame;
+  animation.frameTimer = 0.0f;
+  animation.isPlaying = true;
+}
+
+Rectangle ecs::SpriteAnimationSystem::getCurrentFrameRect(Entity entity) const {
+  const auto& animation = _ecsManager.getComponent<SpriteAnimationComponent>(entity);
+  
+  return {
+    static_cast<float>(animation.currentFrame * animation.frameWidth),
+    static_cast<float>(animation.selectedRow * animation.frameHeight),
+    static_cast<float>(animation.frameWidth),
+    static_cast<float>(animation.frameHeight)
+  };
+}
+
+void ecs::SpriteAnimationSystem::initializeFromTexture(Entity entity, int textureWidth, int textureHeight) {
+  auto& animation = _ecsManager.getComponent<SpriteAnimationComponent>(entity);
+  
+  animation.frameWidth = textureWidth / animation.totalColumns;
+  animation.frameHeight = textureHeight / animation.totalRows;
 }
 
 void ecs::SpriteAnimationSystem::initializeAnimation(Entity entity, Texture2D texture) {
   auto& animation = _ecsManager.getComponent<SpriteAnimationComponent>(entity);
   auto& sprite = _ecsManager.getComponent<SpriteComponent>(entity);
   
-  animation.initializeFromTexture(texture.width, texture.height);
-  sprite.sourceRect = animation.getCurrentFrameRect();
+  initializeFromTexture(entity, texture.width, texture.height);
+  sprite.sourceRect = getCurrentFrameRect(entity);
 }
--- game_engine/ecs/systems/SpriteAnimationSystem.hpp
+++ game_engine/ecs/systems/SpriteAnimationSystem.hpp
@@ -13,10 +13,17 @@ namespace ecs {
         : _ecsManager(ecsManager) {}
 
       void update(float deltaTime) override;
-          
+
       void setSelectedRow(Entity entity, int row);
-      
+      void setAnimationRange(Entity entity, int start, int end);
+      void play(Entity entity);
+      void pause(Entity entity);
+      void stop(Entity entity);
+      void restart(Entity entity);   
+   
       void initializeAnimation(Entity entity, Texture2D texture);
+      void initializeFromTexture(Entity entity, int textureWidth, int textureHeight);
+      Rectangle getCurrentFrameRect(Entity entity) const;
 
     private:
       ECSManager &_ecsManager;
