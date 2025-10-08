1ade84609e1d1f9f9fd47c755ac79edf62f66529
EtienneLbre <etienne.lbre@gmail.com>
2025-10-08 18:31:36 +0200

    fix: include in Client.hpp

--- client/Client.hpp
+++ client/Client.hpp
@@ -13,10 +13,12 @@
     #define PLATFORM_DESKTOP
     #define NOGDI
     #define NOMINMAX
+    #define NOUSER
     #define WIN32_LEAN_AND_MEAN
     #define _WINSOCK_DEPRECATED_NO_WARNINGS
     #define _CRT_SECURE_NO_WARNINGS
     #define _WIN32_WINNT 0x0601
+    #define ASIO_NO_WIN32_LEAN_AND_MEAN
 #endif
 #include <asio.hpp>
 
