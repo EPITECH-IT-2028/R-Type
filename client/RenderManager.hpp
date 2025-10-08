d34765e3249baf1c8b2e5a93c1dd7339f8558c1b
EtienneLbre <etienne.lbre@gmail.com>
2025-10-08 17:55:41 +0200

    fix: CI with windows

--- client/RenderManager.hpp
+++ client/RenderManager.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
 #include <cstdarg>
+#define NOGDI
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#undef Rectangle
+#undef CloseWindow
+#undef ShowCursor
+#undef LoadImage
 #include "raylib.h"
 
 namespace renderManager {
