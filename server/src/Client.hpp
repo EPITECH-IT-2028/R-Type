3049463253cd7b6d344f9091c9b44e7c071b80c5
ZifFiji <arthur.guerinault@epitech.eu>
2025-10-21 12:47:36 +0200

    feat: new attributs to client to start ack

--- server/src/Client.hpp
+++ server/src/Client.hpp
@@ -24,7 +24,9 @@ namespace server {
             _connected(true),
             _room_id(NO_ROOM),
             _last_heartbeat(std::chrono::steady_clock::now()),
-            _last_position_update(std::chrono::steady_clock::now()) {
+            _last_position_update(std::chrono::steady_clock::now()),
+            _last_ack(0),
+            _sequence_number(0) {
       }
       /**
        * @brief Destroys the Client object.
@@ -36,6 +38,8 @@ namespace server {
       bool _connected = false;
       int _player_id = -1;
       uint32_t _room_id = NO_ROOM;
+      std::uint32_t _last_ack = 0;
+      std::uint32_t _sequence_number = 0;
       std::chrono::steady_clock::time_point _last_heartbeat;
       std::chrono::steady_clock::time_point _last_position_update;
       uint32_t _entity_id = std::numeric_limits<uint32_t>::max();
