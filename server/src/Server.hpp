4465ace5390a7a4c61dac62215e073289d771d65
ZifFiji <arthur.guerinault@epitech.eu>
2025-10-29 13:24:57 +0100

    fix: remove windows macros declaration in packets

--- server/src/Server.hpp
+++ server/src/Server.hpp
@@ -1,17 +1,5 @@
 #pragma once
 
-#ifdef _WIN32
-  #ifndef WIN32_LEAN_AND_MEAN
-    #define WIN32_LEAN_AND_MEAN
-  #endif
-  #ifndef NOMINMAX
-    #define NOMINMAX
-  #endif
-  #ifndef _WIN32_WINNT
-    #define _WIN32_WINNT 0x0601
-  #endif
-#endif
-
 #include <asio.hpp>
 #include <cstddef>
 #include <cstdint>
